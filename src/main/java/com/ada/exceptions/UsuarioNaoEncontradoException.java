d1d2c06fd606d7fbc20cce65d9dfa47e5c42bd80
alan.santos <alan.santos@usto.re>
2023-11-06 21:33:31 -0300

    classes de usuario

---
+++ src/main/java/com/ada/exceptions/UsuarioNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.ada.exceptions;
+
+public class UsuarioNaoEncontradoException extends Exception {
+
+    public UsuarioNaoEncontradoException(){
+        super();
+    }
+}
---
+++ src/main/java/com/ada/service/UsuarioService.java
@@ -0,0 +1,34 @@
+package com.ada.service;
+
+import com.ada.exceptions.UsuarioNaoEncontradoException;
+import com.ada.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioService {
+
+    private List<Usuario> usuarios;
+
+    public UsuarioService() {
+        this.popularUsuarios();
+    }
+
+    public void popularUsuarios(){
+        usuarios = new ArrayList<>();
+
+        for (int i=0; i < 5; i++){
+            usuarios.add(new Usuario("user"+i, "user"+i, "pwd"+i));
+        }
+    }
+
+    public Usuario fazerLogin(String login, String senha) throws UsuarioNaoEncontradoException {
+        for (Usuario u : usuarios ) {
+            if (u.getLogin().equals(login) && u.getSenha().equals(senha)){
+                return u;
+            }
+        }
+        throw new UsuarioNaoEncontradoException();
+    }
+
+}
